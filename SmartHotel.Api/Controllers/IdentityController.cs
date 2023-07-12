c099c5a7553dd587e7f12d8182928497fc9b6492
youssef hemdan <youssefhemdan02@gmail.com>
2023-07-12 10:58:23 +0300

    change refershtoken to login add auhtorize to addrole

--- SmartHotel.Api/Controllers/IdentityController.cs
+++ SmartHotel.Api/Controllers/IdentityController.cs
@@ -33,7 +33,7 @@ namespace SmartHotel.Api.Controllers
         }
 
         [HttpPost]
-        [Route("refreshtoken")]
+        [Route("login")]
         public async Task<ActionResult> RefreshToken([FromBody] LoginUserDto request)
         {
             if (!ModelState.IsValid)
@@ -48,7 +48,7 @@ namespace SmartHotel.Api.Controllers
         }
 
         [HttpPost("addrole")]
-        
+        [Authorize(Roles ="Administrator")]
         public async Task<IActionResult> AddRoleAsync([FromBody] AddToRoleDto model)
         {
             if (!ModelState.IsValid)
